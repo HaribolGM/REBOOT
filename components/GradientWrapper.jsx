e9b5fddfd31b5cc33b44d0e13d52bb506aaf6d28
rudrodip <official.rudrodipsarker@gmail.com>
2023-12-03 23:10:51 +0600

    Fix typo in GradientWrapper className

--- components/GradientWrapper.jsx
+++ components/GradientWrapper.jsx
@@ -2,7 +2,7 @@ const GradientWrapper = ({ children, ...props }) => (
     <div
         {...props}
         className={`relative ${props.className || ""}`}>
-        <div className={`absolute m-auto blur-[160px] ${props.wrapperClassName || ""}`}
+        <div className={`absolute m-auto blur-[160px] ${props.wrapperclassname || ""}`}
             style={{
                 background:
                     "linear-gradient(180deg, #7C3AED 0%, rgba(152, 103, 240, 0.984375) 0.01%, rgba(237, 78, 80, 0.2) 100%)",
